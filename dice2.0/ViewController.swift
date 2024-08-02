5925f605f4025fc99fb32d2b93aad2fde09358f8
singlaamitesh <143833521+singlaamitesh@users.noreply.github.com>
2024-08-02 10:00:31 +0530

    Initial Commit

---
+++ dice2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dice2.0
+//
+//  Created by Amitesh Gupta on 02/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
