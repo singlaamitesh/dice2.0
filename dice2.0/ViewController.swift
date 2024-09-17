a723a8ada590d43099ccd2d0b5d3e49434b6d9cc
singlaamitesh <143833521+singlaamitesh@users.noreply.github.com>
2024-09-17 19:22:43 +0530

    Inital Commit

--- dice2.0/ViewController.swift
+++ dice2.0/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var rollbutton: UIButton!
+    @IBOutlet weak var dice1: UIImageView!
+    
+    @IBOutlet weak var dice2: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
-
+    @IBAction func ROLLPressed(_ sender: UIButton) {
+        let dice  = [  #imageLiteral(resourceName: "dice-six-faces-one") , #imageLiteral(resourceName: "dice-six-faces-two") , #imageLiteral(resourceName: "dice-six-faces-three") , #imageLiteral(resourceName: "dice-six-faces-four") , #imageLiteral(resourceName: "dice-six-faces-five") , #imageLiteral(resourceName: "dice-six-faces-six")]
+        dice1.image = dice[Int.random(in: 0...5)]
+        dice2.image = dice[Int.random(in: 0...5)]
+    }
+   
 }
 
